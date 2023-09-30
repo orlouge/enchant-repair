cf48cc978c9186d5d19a4773096216b64b76d1c1
orlouge <orlouge@protonmail.com>
2023-09-30 23:51:24 +0200

    Allow enchanting items with 0 enchantability

--- common/src/main/java/io/github/orlouge/enchantrepair/mixin/EnchantmentScreenHandlerMixin.java
+++ common/src/main/java/io/github/orlouge/enchantrepair/mixin/EnchantmentScreenHandlerMixin.java
@@ -4,12 +4,15 @@ import io.github.orlouge.enchantrepair.Config;
 import io.github.orlouge.enchantrepair.ModifiedEnchantingHelper;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.ChiseledBookshelfBlockEntity;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
 import net.minecraft.screen.*;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
@@ -18,6 +21,7 @@ import net.minecraft.util.Pair;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,8 +29,10 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.*;
 
@@ -34,6 +40,13 @@ import java.util.*;
 public abstract class EnchantmentScreenHandlerMixin extends ScreenHandler {
     @Shadow @Final private Random random;
     @Shadow @Final private ScreenHandlerContext context;
+    @Shadow @Final public int[] enchantmentPower;
+    @Shadow @Final private Property seed;
+
+    @Shadow protected abstract List<EnchantmentLevelEntry> generateEnchantments(ItemStack stack, int slot, int level);
+
+    @Shadow @Final public int[] enchantmentId;
+    @Shadow @Final public int[] enchantmentLevel;
     private int playerLevel = 30;
     private Collection<ModifiedEnchantingHelper.StoredBook> books = null;
     private boolean applyEnchantments = true;
@@ -58,6 +71,32 @@ public abstract class EnchantmentScreenHandlerMixin extends ScreenHandler {
             });
         }
         if (this.books == null) this.updateBooks();
+    }
+    
+    @Inject(method = "onContentChanged", cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD, at = @At(value = "INVOKE", shift = At.Shift.BEFORE, target = "Lnet/minecraft/screen/ScreenHandlerContext;run(Ljava/util/function/BiConsumer;)V"))
+    public void onContentChangedUpdateBookOnly(Inventory inventory, CallbackInfo ci, ItemStack stack) {
+        if (stack.getItem().getEnchantability() <= 0) {
+            this.context.run((world, pos) -> {
+                this.random.setSeed(this.seed.get());
+
+                for (int slot = 0; slot <= 2; slot++) {
+                    this.enchantmentPower[slot] = 1;
+                    List<EnchantmentLevelEntry> enchantments = this.generateEnchantments(stack, slot, 0);
+                    if (enchantments != null && !enchantments.isEmpty()) {
+                        EnchantmentLevelEntry entry = enchantments.get(this.random.nextInt(enchantments.size()));
+                        this.enchantmentId[slot] = Registries.ENCHANTMENT.getRawId(entry.enchantment);
+                        this.enchantmentLevel[slot] = entry.level;
+                    } else {
+                        this.enchantmentPower[slot] = 0;
+                        this.enchantmentId[slot] = -1;
+                        this.enchantmentLevel[slot] = -1;
+                    }
+                }
+
+                this.sendContentUpdates();
+            });
+            ci.cancel();
+        }
     }
 
     private static void consumeBooks(World world, Collection<ModifiedEnchantingHelper.StoredBook> consumedBooks) {
@@ -92,28 +131,34 @@ public abstract class EnchantmentScreenHandlerMixin extends ScreenHandler {
     }
 
     @Inject(method = "generateEnchantments", at = @At(value = "RETURN"),  cancellable = true)
-    public void addBookEnchantments(ItemStack stack, int slot, int level, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
-        if (this.books != null && this.books.size() > 0) {
-            Pair<List<EnchantmentLevelEntry>, List<ModifiedEnchantingHelper.StoredBook>> generated = ModifiedEnchantingHelper.generateEnchantmentsFromBooks(stack, this.books, this.playerLevel, this.random);
-            List<EnchantmentLevelEntry> bookEnchantments = new ArrayList<>(generated.getLeft());
-            List<EnchantmentLevelEntry> randomEnchantments = cir.getReturnValue();
-            for (EnchantmentLevelEntry enchantment : randomEnchantments) {
-                if (bookEnchantments.stream().anyMatch(e -> !e.enchantment.canCombine(enchantment.enchantment)))
-                    continue;
-                bookEnchantments.add(enchantment);
-            }
-            if (applyEnchantments) {
-                this.consumedBooks = generated.getRight();
-                if (stack.getItem().isDamageable() && random.nextDouble() < ModifiedEnchantingHelper.enchantDamageChance(this.playerLevel)) {
-                    int dmg = stack.getDamage(), max = stack.getItem().getMaxDamage();
-                    int newdmg = dmg + (max - dmg) * 3 / 10;
-                    if (newdmg > dmg + 10 && newdmg < max) {
-                        stack.setDamage(newdmg);
-                    }
-                }
-            }
+    public void addBooksToRandomEnchantments(ItemStack stack, int slot, int level, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
+        List<EnchantmentLevelEntry> randomEnchantments = level <= 0 ? Collections.emptyList() : cir.getReturnValue();
+        List<EnchantmentLevelEntry> bookEnchantments = addBookEnchantments(stack, randomEnchantments);
+        if (bookEnchantments != null) {
             cir.setReturnValue(bookEnchantments);
         }
+    }
+
+    private List<EnchantmentLevelEntry> addBookEnchantments(ItemStack stack, List<EnchantmentLevelEntry> randomEnchantments) {
+        if (this.books == null || this.books.size() == 0) return null;
+        Pair<List<EnchantmentLevelEntry>, List<ModifiedEnchantingHelper.StoredBook>> generated = ModifiedEnchantingHelper.generateEnchantmentsFromBooks(stack, this.books, this.playerLevel, this.random);
+        List<EnchantmentLevelEntry> bookEnchantments = new ArrayList<>(generated.getLeft());
+        for (EnchantmentLevelEntry enchantment : randomEnchantments) {
+            if (bookEnchantments.stream().anyMatch(e -> !e.enchantment.canCombine(enchantment.enchantment)))
+                continue;
+            bookEnchantments.add(enchantment);
+        }
+        if (applyEnchantments) {
+            this.consumedBooks = generated.getRight();
+            if (stack.getItem().isDamageable() && random.nextDouble() < ModifiedEnchantingHelper.enchantDamageChance(this.playerLevel)) {
+                int dmg = stack.getDamage(), max = stack.getItem().getMaxDamage();
+                int newdmg = dmg + (max - dmg) * 3 / 10;
+                if (newdmg > dmg + 10 && newdmg < max) {
+                    stack.setDamage(newdmg);
+                }
+            }
+        }
+        return bookEnchantments;
     }
 
     private void updateBooks() {
